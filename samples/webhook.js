947cf742b35c55efda7d8280eb30e8854228d74f
Jaime <jaimeloeuf@gmail.com>
2020-12-21 15:24:28 +0800

    Add sample for using webhook bot:     WIP to be tested.

---
+++ samples/webhook.js
@@ -0,0 +1,18 @@
+/**
+ * Simple echo bot that replies to your message exactly what you sent using telegram's webhook API.
+ */
+
+require("dotenv").config();
+
+const { WebhookBot, shortHands } = require("../src");
+const bot = new WebhookBot(process.env.BOT_TOKEN);
+
+bot.addShortHand(shortHands.replyMessage);
+
+bot.addHandler(function (update) {
+  this.replyMessage(update.message.text, {
+    reply_to_message_id: update.message.message_id,
+  });
+});
+
+bot.setWebhookAndStartServer();
