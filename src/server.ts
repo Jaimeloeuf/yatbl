571249bc7a3c9ecee3fe7b8879232d32f0dc85f9
Jaime <jaimeloeuf@gmail.com>
2022-03-28 15:11:31 +0800

    Cleanup server's API: 1.  Cleanup type annotation of req handler as no return type annotation     is needed since its void. 2.  Change arrow function to regular function to prevent binding anything     out of scope to prevent potential memory leaks.

--- src/server.ts
+++ src/server.ts
@@ -14,56 +14,53 @@ export default (
   PORT: number,
   path: string
 ) =>
-  createServer(
-    // Using arrow function to keep `this` binding of startServer function
-    async (req, res): Promise<any> => {
-      try {
-        // Only handle update if both path and method matches exactly
-        if (req.url === path && req.method === "POST") {
-          // Get the POST request body, which is an Update object
-          const update: Update = await new Promise((resolve, reject) => {
-            const chunks: Array<Uint8Array> = [];
-            req
-              .on("data", (chunk) => chunks.push(chunk))
-              .on("end", () =>
-                resolve(JSON.parse(Buffer.concat(chunks).toString()))
-              )
-              .on("error", reject);
-          });
+  createServer(async function (req, res) {
+    try {
+      // Only handle update if both path and method matches exactly
+      if (req.url === path && req.method === "POST") {
+        // Get the POST request body, which is an Update object
+        const update: Update = await new Promise((resolve, reject) => {
+          const chunks: Array<Uint8Array> = [];
+          req
+            .on("data", (chunk) => chunks.push(chunk))
+            .on("end", () =>
+              resolve(JSON.parse(Buffer.concat(chunks).toString()))
+            )
+            .on("error", reject);
+        });
 
-          // Unlike getUpdates telegram API
-          // 1. Every webhook invocation / HTTP POST message coming in, will only receive ONE update at a time
-          //    Although telegram can make multiple simultaneous POST requests to your bot, each request only ONE update object
-          //    This is why update object is put in an array, since onUpdate always expects an array of update(s)
-          // 2. This is not a response so there is no 'ok' field in the request body from telegram
-          //
-          // References:
-          // https://core.telegram.org/bots/webhooks#testing-your-bot-with-updates
-          // https://core.telegram.org/bots/api#setwebhook
-          // https://core.telegram.org/bots/api#getupdates
-          await bot._onUpdate([update]);
+        // Unlike getUpdates telegram API
+        // 1. Every webhook invocation / HTTP POST message coming in, will only receive ONE update at a time
+        //    Although telegram can make multiple simultaneous POST requests to your bot, each request only ONE update object
+        //    This is why update object is put in an array, since onUpdate always expects an array of update(s)
+        // 2. This is not a response so there is no 'ok' field in the request body from telegram
+        //
+        // References:
+        // https://core.telegram.org/bots/webhooks#testing-your-bot-with-updates
+        // https://core.telegram.org/bots/api#setwebhook
+        // https://core.telegram.org/bots/api#getupdates
+        await bot._onUpdate([update]);
 
-          // Set header to indicate response type as JSON
-          // res.setHeader("Content-Type", "application/json");
-          res.writeHead(200);
-          res.end();
-        } else {
-          // If route is invalid, we assume it is not telegram that called our API
-          // Thus return 404 and dont respond to any updates sent
-          res.writeHead(404);
-          res.end();
-        }
-      } catch (error) {
-        console.error(error);
-
-        // actual error handler instead of the normal api error handler
-        bot._apiErrorHandler(error);
-
-        // Close the connection
-        res.writeHead(500);
+        // Set header to indicate response type as JSON
+        // res.setHeader("Content-Type", "application/json");
+        res.writeHead(200);
+        res.end();
+      } else {
+        // If route is invalid, we assume it is not telegram that called our API
+        // Thus return 404 and dont respond to any updates sent
+        res.writeHead(404);
         res.end();
       }
+    } catch (error) {
+      console.error(error);
+
+      // actual error handler instead of the normal api error handler
+      bot._apiErrorHandler(error);
+
+      // Close the connection
+      res.writeHead(500);
+      res.end();
     }
-  ).listen(PORT, () =>
+  }).listen(PORT, () =>
     console.log(`Webhook server running on localhost:${PORT}`)
   );
